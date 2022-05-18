85550c0d36ef7f4202a02692d8aa0b5fae4e450a
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2022-05-18 09:52:24 +0000

    Creating new service pension-scheme-event-reporting-frontend

---
+++ app/uk/gov/hmrc/pensionschemeeventreportingfrontend/config/AppConfig.scala
@@ -0,0 +1,10 @@
+package uk.gov.hmrc.pensionschemeeventreportingfrontend.config
+
+import javax.inject.{Inject, Singleton}
+import play.api.Configuration
+
+@Singleton
+class AppConfig @Inject()(config: Configuration) {
+  val welshLanguageSupportEnabled: Boolean = config.getOptional[Boolean]("features.welsh-language-support").getOrElse(false)
+
+}
---
+++ conf/messages.cy
@@ -0,0 +1,2 @@
+service.name = pension-scheme-event-reporting-frontend
+service.text = Dyma'ch gwasanaeth newydd
