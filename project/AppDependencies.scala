bd2fb2c5c330ddad6909bc3cd64d073319bcf04e
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2022-05-20 14:54:40 +0100

    PODS-7025 njk, build.sbt configs are added

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,18 +1,33 @@
 import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.17.0-play-28"
+    play.sbt.PlayImport.ws,
+    "uk.gov.hmrc"                   %%  "play-conditional-form-mapping"  % "1.9.0-play-28",
+    "uk.gov.hmrc"                   %%  "bootstrap-frontend-play-28"     % "5.16.0",
+    "uk.gov.hmrc"                   %%  "play-nunjucks"                  % "0.33.0-play-28",
+    "uk.gov.hmrc"                   %%  "play-nunjucks-viewmodel"        % "0.15.0-play-28",
+    "org.webjars.npm"               %   "govuk-frontend"                 % "3.5.0",
+    "org.webjars.npm"               %   "hmrc-frontend"                  % "1.19.0",
+    "com.google.inject.extensions"  %   "guice-multibindings"            % "4.2.2",
+    "uk.gov.hmrc"                   %%  "domain"                         % "6.2.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"             % "test, it",
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it"
-  )
+    "org.scalatest"               %% "scalatest"          % "3.2.3",
+    "org.scalatestplus.play"      %% "scalatestplus-play" % "5.1.0",
+    "org.jsoup"                   %  "jsoup"              % "1.10.3",
+    "com.typesafe.play"           %% "play-test"          % PlayVersion.current,
+    "org.mockito"                 % "mockito-core"        % "4.0.0",
+    "org.mockito"                 %% "mockito-scala"      % "1.17.5",
+    "org.scalacheck"              %% "scalacheck"         % "1.15.2",
+    "org.scalatestplus"           %% "scalatestplus-scalacheck"   % "3.1.0.0-RC2",
+    "com.github.tomakehurst"      %  "wiremock-jre8"      % "2.26.0",
+    "com.vladsch.flexmark"        % "flexmark-all"        % "0.36.8"
+  ).map(_ % Test)
+
+  def apply(): Seq[ModuleID] = compile ++ test
 }
+
