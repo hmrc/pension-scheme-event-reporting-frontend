66ecb749c83640f961e56d119895739d75117c25
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-07-20 07:31:03 +0100

    WIP

--- .g8/yesNoPage/app/controllers/$className$Controller.scala
+++ .g8/yesNoPage/app/controllers/$className$Controller.scala
@@ -6,7 +6,7 @@ import controllers.actions._
 import forms.$className$FormProvider
 import javax.inject.Inject
 import pages.{$className$Page, Waypoints}
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.$className$View
