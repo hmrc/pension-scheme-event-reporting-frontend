1ebd0677879fdf87334d95a8225f6e98a652cbf1
westwater <gmwestwater@hotmail.co.uk>
2022-07-22 16:50:03 +0100

    PODS-7394 Added optional package structure

--- .g8/checkboxPage/generated-test/controllers/$package$/$className$ControllerSpec.scala
+++ .g8/checkboxPage/generated-test/controllers/$package$/$className$ControllerSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package controllers
+package controllers$if(package.empty)$$else$.$package$$endif$
 
 import base.SpecBase
 import connectors.UserAnswersCacheConnector
