548cee6762702cd612c919497aacca6ed095ee31
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-07-29 08:33:05 +0100

    PODS-7424 Refactor for readability

--- app/controllers/actions/IdentifierAction.scala
+++ app/controllers/actions/IdentifierAction.scala
@@ -51,16 +51,6 @@ class AuthenticatedIdentifierAction @Inject()(
   private val enrolmentPSA = "HMRC-PODS-ORG"
   private val enrolmentPSP = "HMRC-PODSPP-ORG"
 
-  private def bothPsaAndPspEnrolmentsPresent(enrolments: Enrolments): Boolean =
-    enrolments.getEnrolment(enrolmentPSA).isDefined && enrolments.getEnrolment(enrolmentPSP).isDefined
-
-  private def getLoggedInUser(externalId: String, role: AdministratorOrPractitioner, enrolments: Enrolments): Option[LoggedInUser] = role match {
-    case Administrator => getPsaId(enrolments).map(LoggedInUser(externalId, Administrator, _))
-    case Practitioner => getPspId(enrolments).map(LoggedInUser(externalId, Practitioner, _))
-  }
-
-  private def futureUnauthorisedPage: Future[Result] = Future.successful(Redirect(routes.UnauthorisedController.onPageLoad))
-
   override def invokeBlock[A](
                                request: Request[A],
                                block: IdentifierRequest[A] => Future[Result]
@@ -73,24 +63,11 @@ class AuthenticatedIdentifierAction @Inject()(
       Retrievals.externalId and Retrievals.allEnrolments
     ) {
       case Some(externalId) ~ enrolments if bothPsaAndPspEnrolmentsPresent(enrolments) =>
-        administratorOrPractitioner(externalId).flatMap {
-          case None => Future.successful(Redirect(Call("GET", config.administratorOrPractitionerUrl)))
-          case Some(role) =>
-            getLoggedInUser(externalId, role, enrolments) match {
-              case Some(loggedInUser) => block(IdentifierRequest(request, loggedInUser))
-              case _ => futureUnauthorisedPage
-            }
-        }
+        actionForBothEnrolments(externalId, enrolments, request, block)
       case Some(externalId) ~ enrolments if enrolments.getEnrolment(enrolmentPSA).isDefined =>
-        getLoggedInUser(externalId, Administrator, enrolments) match {
-          case Some(loggedInUser) => block(IdentifierRequest(request, loggedInUser))
-          case _ => futureUnauthorisedPage
-        }
+        actionForOneEnrolment(Administrator, externalId, enrolments, request, block)
       case Some(externalId) ~ enrolments if enrolments.getEnrolment(enrolmentPSP).isDefined =>
-        getLoggedInUser(externalId, Practitioner, enrolments) match {
-          case Some(loggedInUser) => block(IdentifierRequest(request, loggedInUser))
-          case _ => futureUnauthorisedPage
-        }
+        actionForOneEnrolment(Practitioner, externalId, enrolments, request, block)
       case _ => futureUnauthorisedPage
     } recover {
       case _: NoActiveSession =>
@@ -118,6 +95,37 @@ class AuthenticatedIdentifierAction @Inject()(
       optionJsValue.flatMap { json =>
         (json \ "administratorOrPractitioner").toOption.flatMap(_.validate[AdministratorOrPractitioner].asOpt)
       }
+    }
+  }
+
+  private def bothPsaAndPspEnrolmentsPresent(enrolments: Enrolments): Boolean =
+    enrolments.getEnrolment(enrolmentPSA).isDefined && enrolments.getEnrolment(enrolmentPSP).isDefined
+
+  private def getLoggedInUser(externalId: String, role: AdministratorOrPractitioner, enrolments: Enrolments): Option[LoggedInUser] = role match {
+    case Administrator => getPsaId(enrolments).map(LoggedInUser(externalId, Administrator, _))
+    case Practitioner => getPspId(enrolments).map(LoggedInUser(externalId, Practitioner, _))
+  }
+
+  private def futureUnauthorisedPage: Future[Result] = Future.successful(Redirect(routes.UnauthorisedController.onPageLoad))
+
+  private def actionForBothEnrolments[A](externalId: String, enrolments: Enrolments, request: Request[A],
+                                         block: IdentifierRequest[A] => Future[Result])(implicit headerCarrier: HeaderCarrier): Future[Result] = {
+    administratorOrPractitioner(externalId).flatMap {
+      case None => Future.successful(Redirect(Call("GET", config.administratorOrPractitionerUrl)))
+      case Some(role) =>
+        getLoggedInUser(externalId, role, enrolments) match {
+          case Some(loggedInUser) => block(IdentifierRequest(request, loggedInUser))
+          case _ => futureUnauthorisedPage
+        }
+    }
+  }
+
+  def actionForOneEnrolment[A](administratorOrPractitioner: AdministratorOrPractitioner,
+                               externalId: String, enrolments: Enrolments, request: Request[A],
+                               block: IdentifierRequest[A] => Future[Result])(implicit headerCarrier: HeaderCarrier): Future[Result] = {
+    getLoggedInUser(externalId, administratorOrPractitioner, enrolments) match {
+      case Some(loggedInUser) => block(IdentifierRequest(request, loggedInUser))
+      case _ => futureUnauthorisedPage
     }
   }
 }
