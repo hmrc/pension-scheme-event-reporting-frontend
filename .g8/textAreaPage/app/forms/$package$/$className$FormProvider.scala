f8e087913749ae87065a18a6a06b8decc8688260
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-09-29 14:58:39 +0100

    text area add to scaffold

---
+++ .g8/textAreaPage/app/forms/$package$/$className$FormProvider.scala
@@ -0,0 +1,15 @@
+package forms$if(package.empty)$$else$.$package$$endif$
+
+import javax.inject.Inject
+
+import forms.mappings.Mappings
+import play.api.data.Form
+
+class $className$FormProvider @Inject() extends Mappings {
+
+  def apply(): Form[Option[String]] =
+    Form(
+      "value" -> optionalText()
+        .verifying(maxLength(150, "$className$.error.length"))
+    )
+}
