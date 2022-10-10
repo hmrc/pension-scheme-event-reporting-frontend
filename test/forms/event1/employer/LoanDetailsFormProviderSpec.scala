9a156906ab9c2f562b17cdd5dbc4edc7b1a90c05
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-10-10 07:12:29 +0100

    WIP

--- test/forms/event1/employer/LoanDetailsFormProviderSpec.scala
+++ test/forms/event1/employer/LoanDetailsFormProviderSpec.scala
@@ -42,19 +42,22 @@ class LoanDetailsFormProviderSpec extends StringFieldBehaviours {
   private val fundValueKey = "fundValue"
 
   private val loanAmountNotANumberErrorKey = "loanDetails.loanAmount.notANumber"
-//  private val loanAmountNoDecimalsKey = "loanDetails.loanAmount.noDecimals"
+  private val loanAmountNoDecimalsKey = "loanDetails.loanAmount.noDecimals"
   private val loanAmountAmountTooHighErrorKey = "loanDetails.loanAmount.amountTooHigh"
+
+  private val fundValueNotANumberErrorKey = "loanDetails.fundValue.notANumber"
+  private val fundValueNoDecimalsKey = "loanDetails.fundValue.noDecimals"
+  private val fundValueAmountTooHighErrorKey = "loanDetails.fundValue.amountTooHigh"
 
   private def details(loanAmount: String = "12",
                       fundValue: String = "1.00"
-                      ): Map[String, String] =
+                     ): Map[String, String] =
     Map(
       loanAmountKey -> loanAmount,
       fundValueKey -> fundValue
     )
 
   "loanAmount" - {
-
     "not bind non-numeric numbers" in {
       forAll(nonNumerics -> "nonNumeric") {
         nonNumeric: String =>
@@ -64,43 +67,74 @@ class LoanDetailsFormProviderSpec extends StringFieldBehaviours {
     }
 
     "not bind decimals that are greater than 2 dp" in {
-          forAll(decimals -> "decimal") {
-            decimal: String =>
-              val result = form.bind(details(loanAmount = decimal))
-              result.errors mustEqual Seq(FormError(loanAmountKey, loanAmountAmountTooHighErrorKey))
-          }
-        }
-//
-//    "not bind decimals below 0.00" in {
-//      forAll(decimalsBelowValue(BigDecimal("0.00")) -> "decimalBelowMin") {
-//        decimal: String =>
-//          val result = form.bind(details(loanAmount = decimal))
-//          result.errors.head.key mustEqual totalAmtOfTaxDueAtHigherRateKey
-//          result.errors.head.message mustEqual messages(s"$messageKeyAmountTaxDueHigherRateKey.error.minimum", "0.01")
-//      }
-//    }
-//
-//    "not bind decimals longer than 11 characters" in {
-//      forAll(longDecimalString(12) -> "decimalAboveMax") {
-//        decimal: String =>
-//          val result = form.bind(details(loanAmount = decimal))
-//          result.errors.head.key mustEqual totalAmtOfTaxDueAtHigherRateKey
-//          result.errors.head.message mustEqual s"$messageKeyAmountTaxDueHigherRateKey.error.maximum"
-//      }
-//    }
-//
-//    "bind 0.00 when positive value bound to totalAmtOfTaxDueAtLowerRate" in {
-//      val result = form.bind(details(loanAmount = "0.00"))
-//      result.errors mustBe Seq.empty
-//    }
-//
-//    "bind integers to totalAmtOfTaxDueAtHigherRate" in {
-//      forAll(intsAboveValue(0) -> "intAboveMax") {
-//        i: Int =>
-//          val result = form.bind(details(loanAmount = i.toString))
-//          result.errors mustBe Seq.empty
-//          result.value.flatMap(_.totalAmtOfTaxDueAtHigherRate) mustBe Some(BigDecimal(i))
-//      }
+      forAll(decimals -> "decimal") {
+        decimal: String =>
+          val result = form.bind(details(loanAmount = decimal))
+          result.errors mustEqual Seq(FormError(loanAmountKey, loanAmountNoDecimalsKey))
+      }
     }
+
+    "not bind decimals longer than 11 characters" in {
+      forAll(longDecimalString(12) -> "decimalAboveMax") {
+        decimal: String =>
+          val result = form.bind(details(loanAmount = decimal))
+          result.errors.headOption.map(_.message) mustEqual Some(loanAmountAmountTooHighErrorKey)
+      }
+    }
+
+    "bind 0.00 when positive value bound to totalAmtOfTaxDueAtLowerRate" in {
+      val result = form.bind(details(loanAmount = "0.00"))
+      result.errors mustBe Seq.empty
+    }
+
+    "bind integers to totalAmtOfTaxDueAtHigherRate" in {
+      forAll(intsInRange(0, 999999) -> "intInRange") {
+        i =>
+          val result = form.bind(details(loanAmount = i))
+          result.errors mustBe Seq.empty
+          result.value.flatMap(_.loanAmount) mustBe Some(BigDecimal(i))
+      }
+    }
+  }
+
+  "fundValue" - {
+    "not bind non-numeric numbers" in {
+      forAll(nonNumerics -> "nonNumeric") {
+        nonNumeric: String =>
+          val result = form.bind(details(fundValue = nonNumeric))
+          result.errors mustEqual Seq(FormError(fundValueKey, fundValueNotANumberErrorKey))
+      }
+    }
+
+    "not bind decimals that are greater than 2 dp" in {
+      forAll(decimals -> "decimal") {
+        decimal: String =>
+          val result = form.bind(details(fundValue = decimal))
+          result.errors mustEqual Seq(FormError(fundValueKey, fundValueNoDecimalsKey))
+      }
+    }
+
+    "not bind decimals longer than 11 characters" in {
+      forAll(longDecimalString(12) -> "decimalAboveMax") {
+        decimal: String =>
+          val result = form.bind(details(fundValue = decimal))
+          result.errors.headOption.map(_.message) mustEqual Some(fundValueAmountTooHighErrorKey)
+      }
+    }
+
+    "bind 0.00 when positive value bound to totalAmtOfTaxDueAtLowerRate" in {
+      val result = form.bind(details(fundValue = "0.00"))
+      result.errors mustBe Seq.empty
+    }
+
+    "bind integers to totalAmtOfTaxDueAtHigherRate" in {
+      forAll(intsInRange(0, 999999) -> "intInRange") {
+        i =>
+          val result = form.bind(details(fundValue = i))
+          result.errors mustBe Seq.empty
+          result.value.flatMap(_.fundValue) mustBe Some(BigDecimal(i))
+      }
+    }
+  }
 }
 
