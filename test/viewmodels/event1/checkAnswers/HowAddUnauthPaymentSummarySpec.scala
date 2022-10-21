d983e301d274022e95f9cbba45040f746702d295
Shehzad Ismail <shehzad.ismail@atos.net>
2022-10-21 10:30:42 +0100

    [PODS-7612: Created Spec file for HowAddUnauthPaymentSummary]

---
+++ test/viewmodels/event1/checkAnswers/HowAddUnauthPaymentSummarySpec.scala
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.event1.checkAnswers
+
+import base.SpecBase
+import models.UserAnswers
+import models.enumeration.EventType.Event1
+import models.event1.HowAddUnauthPayment
+import pages.event1.HowAddUnauthPaymentPage
+import pages.{CheckAnswersPage, CheckYourAnswersPage, EmptyWaypoints, Waypoints}
+import play.twirl.api.HtmlFormat
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
+import viewmodels.govuk.SummaryListFluency
+import viewmodels.implicits._
+
+
+class HowAddUnauthPaymentSummarySpec extends SpecBase with SummaryListFluency {
+
+
+  "row" - {
+
+    "must display correct information for who received the unauthorised payment or deemed unauthorised payment (Manual)" in {
+
+      val answer = UserAnswers().setOrException(HowAddUnauthPaymentPage, HowAddUnauthPayment.Manual)
+      val waypoints: Waypoints = EmptyWaypoints
+      val sourcePage: CheckAnswersPage = CheckYourAnswersPage(Event1)
+
+      val value = ValueViewModel(
+        HtmlContent(
+          HtmlFormat.escape(messages(s"howAddUnauthPayment.${HowAddUnauthPayment.Manual}"))
+        )
+      )
+
+      HowAddUnauthPaymentSummary.row(answer, waypoints, sourcePage) mustBe Some(
+        SummaryListRowViewModel(
+          key = "howAddUnauthPayment.checkYourAnswersLabel",
+          value = value,
+          actions = Seq(
+            ActionItemViewModel("site.change", HowAddUnauthPaymentPage.changeLink(waypoints, sourcePage).url)
+              .withVisuallyHiddenText(messages("howAddUnauthPayment.change.hidden"))
+          )
+        )
+      )
+    }
+
+    "must display correct information for who received the unauthorised payment or deemed unauthorised payment (FileUpload)" in {
+
+      val answer = UserAnswers().setOrException(HowAddUnauthPaymentPage, HowAddUnauthPayment.FileUpload)
+      val waypoints: Waypoints = EmptyWaypoints
+      val sourcePage: CheckAnswersPage = CheckYourAnswersPage(Event1)
+
+      val value = ValueViewModel(
+        HtmlContent(
+          HtmlFormat.escape(messages(s"howAddUnauthPayment.${HowAddUnauthPayment.FileUpload}"))
+        )
+      )
+
+      HowAddUnauthPaymentSummary.row(answer, waypoints, sourcePage) mustBe Some(
+        SummaryListRowViewModel(
+          key = "howAddUnauthPayment.checkYourAnswersLabel",
+          value = value,
+          actions = Seq(
+            ActionItemViewModel("site.change", HowAddUnauthPaymentPage.changeLink(waypoints, sourcePage).url)
+              .withVisuallyHiddenText(messages("howAddUnauthPayment.change.hidden"))
+          )
+        )
+      )
+    }
+  }
+}
