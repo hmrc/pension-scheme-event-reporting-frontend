3a18597df9377ef48fd618aba32f8acb0fcf7c20
bonnie.brown <bonnie.brown@digital.hmrc.gov.uk>
2022-11-21 09:38:21 +0000

    WIP

---
+++ app/controllers/event22/Event22CheckYourAnswersController.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.event22
+
+import com.google.inject.Inject
+import connectors.EventReportingConnector
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import models.enumeration.EventType.Event22
+import models.requests.DataRequest
+import pages.event22.Event22CheckYourAnswersPage
+import pages.{CheckAnswersPage, EmptyWaypoints, Waypoints}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.{ChooseTaxYearSummary, MembersDetailsSummary, TotalPensionAmountsSummary}
+import viewmodels.govuk.summarylist._
+
+import scala.concurrent.ExecutionContext
+
+class Event22CheckYourAnswersController @Inject()(
+                                                   override val messagesApi: MessagesApi,
+                                                   identify: IdentifierAction,
+                                                   getData: DataRetrievalAction,
+                                                   requireData: DataRequiredAction,
+                                                   connector: EventReportingConnector,
+                                                   val controllerComponents: MessagesControllerComponents,
+                                                   view: CheckYourAnswersView
+                                                 )(implicit val ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(): Action[AnyContent] =
+    (identify andThen getData(Event22) andThen requireData) { implicit request =>
+      val thisPage = Event22CheckYourAnswersPage
+      val waypoints = EmptyWaypoints
+      val continueUrl = controllers.event22.routes.Event22CheckYourAnswersController.onClick.url
+      Ok(view(SummaryListViewModel(rows = buildEvent22CYARows(waypoints, thisPage)), continueUrl))
+    }
+
+  /**
+   * TODO: replace controllers.event22.routes.Event22CheckYourAnswersController with actual event22.SummaryPageController
+   */
+  def onClick: Action[AnyContent] =
+    (identify andThen getData(Event22) andThen requireData).async { implicit request =>
+      connector.compileEvent("123", Event22).map {
+        _ =>
+          Redirect(controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad().url)
+      }
+    }
+
+  private def buildEvent22CYARows(waypoints: Waypoints, sourcePage: CheckAnswersPage)
+                                 (implicit request: DataRequest[AnyContent]): Seq[SummaryListRow] = {
+    MembersDetailsSummary.rowFullName(request.userAnswers, waypoints, None, sourcePage, Event22).toSeq ++
+      MembersDetailsSummary.rowNino(request.userAnswers, waypoints, None, sourcePage, Event22).toSeq ++
+      ChooseTaxYearSummary.row(request.userAnswers, waypoints, sourcePage, Event22).toSeq ++
+      TotalPensionAmountsSummary.row(request.userAnswers, waypoints, sourcePage, Event22).toSeq
+  }
+}
---
+++ test/controllers/event22/Event22CheckYourAnswersControllerSpec.scala
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.event22
+
+import base.SpecBase
+import data.SampleData.sampleMemberJourneyDataEvent22
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
+import play.api.i18n.Messages
+import play.api.inject
+import play.api.inject.guice.GuiceableModule
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import uk.gov.hmrc.govukfrontend.views.Aliases
+import uk.gov.hmrc.govukfrontend.views.Aliases._
+import viewmodels.govuk.SummaryListFluency
+import views.html.CheckYourAnswersView
+
+class Event22CheckYourAnswersControllerSpec extends SpecBase with SummaryListFluency {
+
+  import Event22CheckYourAnswersControllerSpec._
+
+  "Check Your Answers Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[CheckYourAnswersView]
+        val list = SummaryListViewModel(Seq.empty)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(list, "/manage-pension-scheme-event-report/new-report/event-22-click")(request, messages(application)).toString
+      }
+    }
+
+    "must return OK and the correct summary list row items for a GET (member)" in {
+      val mockView = mock[CheckYourAnswersView]
+      val extraModules: Seq[GuiceableModule] = Seq[GuiceableModule](
+        inject.bind[CheckYourAnswersView].toInstance(mockView)
+      )
+
+      val application = applicationBuilder(
+        userAnswers = Some(sampleMemberJourneyDataEvent22),
+        extraModules = extraModules
+      ).build()
+
+      val captor: ArgumentCaptor[SummaryList] =
+        ArgumentCaptor.forClass(classOf[SummaryList])
+
+      running(application) {
+        when(mockView.apply(captor.capture(), any())(any(), any())).thenReturn(play.twirl.api.Html(""))
+        val request = FakeRequest(GET, controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad().url)
+        val result = route(application, request).value
+        status(result) mustEqual OK
+
+        val actual: Seq[SummaryListRow] = captor.getValue.rows
+        val expected: Seq[Aliases.SummaryListRow] = expectedMemberSummaryListRowsEvent22
+
+        actual.size mustBe expected.size
+
+        actual.zipWithIndex.map { case (a, i) =>
+          a mustBe expected(i)
+        }
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
+
+object Event22CheckYourAnswersControllerSpec {
+
+  private def fakeSummaryListRowWithHtmlContent(messageKey: String, htmlContent: String, changeLink: String)
+                                               (implicit messages: Messages): SummaryListRow =
+    SummaryListRow(
+      Key(
+        Text(
+          messages(messageKey)
+        ), ""),
+      Value(HtmlContent(htmlContent), ""), "",
+      Some(Actions("", List(ActionItem(changeLink, Text("Change"), Some(messages(messageKey)), "", Map()))))
+    )
+
+  private def fakeSummaryListRowWithHtmlContentWithHiddenContent(messageKey: String, htmlContent: String, changeLink: String, hiddenContentChangeLink: String)
+                                                                (implicit messages: Messages): SummaryListRow =
+    SummaryListRow(
+      Key(
+        Text(
+          messages(messageKey)
+        ), ""),
+      Value(HtmlContent(htmlContent), ""), "",
+      Some(Actions("", List(ActionItem(changeLink, Text("Change"), Some(messages(hiddenContentChangeLink)), "", Map()))))
+    )
+
+  private def expectedMemberSummaryListRowsEvent22(implicit messages: Messages): Seq[SummaryListRow] = Seq(
+    fakeSummaryListRowWithHtmlContent(
+      "membersDetails.checkYourAnswersLabel",
+      "Joe Bloggs",
+      "/manage-pension-scheme-event-report/new-report/event-22-member-details?waypoints=event-22-check-your-answers"
+    ),
+    fakeSummaryListRowWithHtmlContent(
+      "membersDetails.checkYourAnswersLabel.nino",
+      "AA224567V",
+      "/manage-pension-scheme-event-report/new-report/event-22-member-details?waypoints=event-22-check-your-answers"
+    ),
+    fakeSummaryListRowWithHtmlContent(
+      "chooseTaxYear.event22.checkYourAnswersLabel",
+      "2015 to 2016",
+      "/manage-pension-scheme-event-report/new-report/event-22-tax-year?waypoints=event-22-check-your-answers"
+    ),
+    fakeSummaryListRowWithHtmlContentWithHiddenContent(
+      "totalPensionAmounts.checkYourAnswersLabel",
+      "£1,224.56",
+      "/manage-pension-scheme-event-report/new-report/event-22-total-input-amount?waypoints=event-22-check-your-answers",
+      "totalPensionAmounts.change.hidden"
+    )
+  )
+}
