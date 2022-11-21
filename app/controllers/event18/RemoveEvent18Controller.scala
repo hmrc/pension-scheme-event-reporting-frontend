9d9eea974b41bea2572ed1879d9068690cc9e0c5
Mary1-5 <marymansha15@gmail.com>
2022-11-21 15:16:48 +0000

    adding correct event type in controller to render page

--- app/controllers/event18/RemoveEvent18Controller.scala
+++ app/controllers/event18/RemoveEvent18Controller.scala
@@ -41,7 +41,7 @@ class RemoveEvent18Controller @Inject()(
                                  )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   private val form = formProvider()
-  private val eventType = EventType.Event1
+  private val eventType = EventType.Event18
 
   def onPageLoad(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData(eventType) andThen requireData) { implicit request =>
     val preparedForm = request.userAnswers.get(RemoveEvent18Page).fold(form)(form.fill)
