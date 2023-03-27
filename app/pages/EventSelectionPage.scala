f0800d09712cd861b785a90ac85dea505c3a1616
Shehzad Ismail <shehzad.ismail@atos.net>
2023-03-27 10:31:37 +0100

    [PODS-8136: Added navigation from event selction to event 8A + updated Specs]

--- app/pages/EventSelectionPage.scala
+++ app/pages/EventSelectionPage.scala
@@ -42,6 +42,7 @@ case object EventSelectionPage extends QuestionPage[EventSelection] {
       case Some(Event1) => ManualOrUploadPage(Event1, answers.countAll(MembersOrEmployersPage(Event1)))
       case Some(Event6) => ManualOrUploadPage(Event6, answers.countAll(MembersPage(Event6)))
       case Some(Event8) => MembersDetailsPage(Event8, answers.countAll(MembersPage(Event8)))
+      case Some(Event8A) => MembersDetailsPage(Event8A, answers.countAll(MembersPage(Event8A)))
       case Some(Event18) => Event18ConfirmationPage
       case Some(Event22) => ManualOrUploadPage(Event22, answers.countAll(MembersPage(Event22)))
       case Some(Event23) => ManualOrUploadPage(Event23, answers.countAll(MembersPage(Event23)))
