f075be35e0bec321b1f5db1eb72409e80f01ce21
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2023-04-03 16:02:17 +0100

    PODS-8136: Split up CYA 8A event update redirect

--- app/controllers/event8a/Event8ACheckYourAnswersController.scala
+++ app/controllers/event8a/Event8ACheckYourAnswersController.scala
@@ -20,6 +20,7 @@ import com.google.inject.Inject
 import connectors.EventReportingConnector
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import models.enumeration.EventType.Event8A
+import models.event8a.PaymentType
 import models.requests.DataRequest
 import models.{Index, UserAnswers}
 import pages.common.MembersDetailsPage
@@ -48,26 +49,59 @@ class Event8ACheckYourAnswersController @Inject()(
                                                  )(implicit val ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
 
   private def missingDataResult(userAnswers: UserAnswers, index:Index, waypoints: Waypoints) = {
+    def missingDataStandAloneLumpSumPage = {
+      val typeOfProtection = pages.event8.TypeOfProtectionPage(Event8A, index)
+      val typeOfProtectionRef = pages.event8.TypeOfProtectionReferencePage(Event8A, index)
+
+      val requiredData = Seq(
+        typeOfProtection -> userAnswers.get(typeOfProtection),
+        typeOfProtectionRef -> userAnswers.get(typeOfProtectionRef)
+      )
+
+      requiredData.collectFirst {
+        case (page, None) => page
+      }
+    }
+
+    def missingDataSchemeSpecificLumpSumPage = {
+      val page  = pages.event8.LumpSumAmountAndDatePage(Event8A, index)
+      userAnswers.get(page) match {
+        case None => Some(page)
+        case _ => None
+      }
+    }
+
+
     val details = MembersDetailsPage(Event8A, index)
     val paymentType = pages.event8a.PaymentTypePage(Event8A, index)
-    val typeOfProtection = pages.event8.TypeOfProtectionPage(Event8A, index)
-    val typeOfProtectionRef = pages.event8.TypeOfProtectionReferencePage(Event8A, index)
+    lazy val paymentTypeUserAnswers = userAnswers.get(paymentType)
 
     val requiredData = Seq(
       details -> userAnswers.get(details),
-      paymentType ->userAnswers.get(paymentType),
-      typeOfProtection -> userAnswers.get(typeOfProtection),
-      typeOfProtectionRef -> userAnswers.get(typeOfProtectionRef)
+      paymentType -> paymentTypeUserAnswers
     )
 
     val page = requiredData.collectFirst {
       case (page, None) => page
     }
 
-    page.map { page =>
+    val finalPage = page match {
+      case None => paymentTypeUserAnswers.flatMap({ paymentType =>
+          if(paymentType == PaymentType.PaymentOfAStandAloneLumpSum) {
+            missingDataStandAloneLumpSumPage
+          } else {
+            missingDataSchemeSpecificLumpSumPage
+          }
+        })
+      case _ => page
+    }
+
+    finalPage.map { page =>
       Redirect(page.changeLink(waypoints, Event8ACheckYourAnswersPage(index)).route)
     }
   }
+
+
 
   def onPageLoad(index: Index): Action[AnyContent] =
     (identify andThen getData(Event8A) andThen requireData) { implicit request =>
