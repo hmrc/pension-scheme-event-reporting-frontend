65d6dd5829ce3978ed26dc81b5a9962d30c911a5
brownbonnie <bonnie.brown@digital.hmrc.gov.uk>
2023-04-05 15:31:45 +0100

    PODS-8011: Created first page

---
+++ app/controllers/WantToSubmitController.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import connectors.UserAnswersCacheConnector
+import controllers.actions._
+import forms.WantToSubmitFormProvider
+import models.enumeration.EventType
+import pages.Waypoints
+import pages.WantToSubmitPage
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.WantToSubmitView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class WantToSubmitController @Inject()(
+                                                  val controllerComponents: MessagesControllerComponents,
+                                                  identify: IdentifierAction,
+                                                  getData: DataRetrievalAction,
+                                                  requireData: DataRequiredAction,
+                                                  userAnswersCacheConnector: UserAnswersCacheConnector,
+                                                  formProvider: WantToSubmitFormProvider,
+                                                  view: WantToSubmitView
+                                                )(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport {
+
+  private val form = formProvider()
+  private val eventType = EventType.Event1
+
+  def onPageLoad(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData(eventType) andThen requireData) { implicit request =>
+    val preparedForm = request.userAnswers.get(WantToSubmitPage()).fold(form)(form.fill)
+    Ok(view(preparedForm, waypoints))
+  }
+
+  def onSubmit(waypoints: Waypoints): Action[AnyContent] = (identify andThen getData(eventType) andThen requireData).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        formWithErrors => Future.successful(BadRequest(view(formWithErrors, waypoints))),
+        value => {
+          val originalUserAnswers = request.userAnswers
+          val updatedAnswers = originalUserAnswers.setOrException(WantToSubmitPage(), value)
+          userAnswersCacheConnector.save(request.pstr, eventType, updatedAnswers).map { _ =>
+            Redirect(WantToSubmitPage().navigate(waypoints, originalUserAnswers, updatedAnswers).route)
+          }
+        }
+      )
+  }
+}
