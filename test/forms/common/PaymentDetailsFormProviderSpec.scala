2fc1c624a816d694b3b292c20ee0fe156b61a6b8
Shehzad Ismail <shehzad.ismail@atos.net>
2023-04-05 15:51:42 +0100

    [PODS-8120: Added PaymentDetailsFormProvider and Page Spec files]

---
+++ test/forms/common/PaymentDetailsFormProviderSpec.scala
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.common
+
+import base.SpecBase
+import forms.behaviours.{BigDecimalFieldBehaviours, DateBehavioursTrait}
+import org.scalacheck.Gen
+import play.api.data.FormError
+import utils.DateHelper.formatDateDMY
+
+import java.time.LocalDate
+
+class PaymentDetailsFormProviderSpec extends SpecBase
+  with BigDecimalFieldBehaviours with DateBehavioursTrait {
+
+  private val stubMin: LocalDate = LocalDate.of(2006, 4, 6)
+  private val stubMax: LocalDate = LocalDate.of(2024, 4, 5)
+
+  private val form = new PaymentDetailsFormProvider().apply(min = stubMin, max = stubMax)
+
+  private val amountPaidKey = "amountPaid"
+  private val eventDateKey = "eventDate"
+  private val messageKeyAmountPaidKey = "paymentDetails.value"
+
+  // scalastyle:off magic.number
+  val invalidDataGenerator: Gen[String] = intsInRangeWithCommas(0, 999999999)
+  val negativeValueDataGenerator: Gen[String] = decimalsBelowValue(0)
+
+  private def paymentDetails(
+                              amountPaid: String,
+                              eventDate: Option[LocalDate]
+                            ): Map[String, String] = eventDate match {
+    case Some(date) => Map(
+      amountPaidKey -> amountPaid,
+      eventDateKey + ".day" -> s"${date.getDayOfMonth}",
+      eventDateKey + ".month" -> s"${date.getMonthValue}",
+      eventDateKey + ".year" -> s"${date.getYear}"
+    )
+    case None => Map(
+      amountPaidKey -> amountPaid
+    )
+  }
+
+  "amountPaid" - {
+
+    "not bind no input" in {
+      val result = form.bind(paymentDetails(amountPaid = "", Some(LocalDate.now())))
+      result.errors mustEqual Seq(FormError(amountPaidKey, s"$messageKeyAmountPaidKey.error.nothingEntered"))
+    }
+
+    "not bind non-numeric numbers" in {
+      val result = form.bind(paymentDetails(amountPaid = "one,two.three", Some(LocalDate.now())))
+      result.errors mustEqual Seq(FormError(amountPaidKey, s"$messageKeyAmountPaidKey.error.notANumber"))
+    }
+
+    "not bind integers" in {
+      forAll(invalidDataGenerator -> "noDecimals") {
+        int: String =>
+          val result = form.bind(paymentDetails(amountPaid = int, Some(LocalDate.now())))
+          result.errors mustEqual Seq(FormError(amountPaidKey, s"$messageKeyAmountPaidKey.error.noDecimals"))
+      }
+    }
+
+    "bind within the range 0 to 999999999.99" in {
+      val number = "999999999.99"
+      val result = form.bind(paymentDetails(amountPaid = number, Some(LocalDate.now())))
+      result.errors mustEqual Nil
+    }
+
+    "not bind outside the range 0 to 999999999.99" in {
+      val number = "1000000000.00"
+      val result = form.bind(paymentDetails(amountPaid = number, Some(LocalDate.now())))
+      result.errors.headOption.map(_.message) mustEqual Some(s"$messageKeyAmountPaidKey.error.amountTooHigh")
+    }
+
+    "not bind numbers equal to 0" in {
+      val number = "0.00"
+      val result = form.bind(paymentDetails(amountPaid = number, Some(LocalDate.now())))
+      result.errors.headOption.map(_.message) mustEqual Some(s"$messageKeyAmountPaidKey.error.zeroEntered")
+    }
+
+    "not bind numbers below 0" in {
+      forAll(negativeValueDataGenerator -> "negative") {
+        number: String =>
+          val result = form.bind(paymentDetails(amountPaid = number, Some(LocalDate.now())))
+          result.errors.headOption.map(_.message) mustEqual Some(s"$messageKeyAmountPaidKey.error.negativeValue")
+      }
+    }
+  }
+
+  "eventDate" - {
+
+    behave like mandatoryDateField(
+      form = form,
+      key = eventDateKey,
+      requiredAllKey = "paymentDetails.date.error.nothingEntered"
+    )
+
+    behave like dateFieldYearNot4Digits(
+      form = form,
+      key = eventDateKey,
+      formError = FormError(eventDateKey, "paymentDetails.date.error.outsideDateRanges")
+    )
+
+    behave like dateFieldWithMin(
+      form = form,
+      key = eventDateKey,
+      min = stubMin,
+      formError = FormError(eventDateKey, messages("paymentDetails.date.error.outsideReportedYear", formatDateDMY(stubMin), formatDateDMY(stubMax)))
+    )
+
+    behave like dateFieldWithMax(
+      form = form,
+      key = eventDateKey,
+      max = stubMax,
+      formError = FormError(eventDateKey, messages("paymentDetails.date.error.outsideReportedYear", formatDateDMY(stubMin), formatDateDMY(stubMax)))
+    )
+  }
+}
