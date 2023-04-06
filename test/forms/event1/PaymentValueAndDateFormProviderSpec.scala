33394337817e9f58284f4b3a8d8ff70d6f1ac8fc
Mary1-5 <marymansha15@gmail.com>
2023-04-06 15:11:15 +0100

    PODS-8273 adjusting tests

--- test/forms/event1/PaymentValueAndDateFormProviderSpec.scala
+++ test/forms/event1/PaymentValueAndDateFormProviderSpec.scala
@@ -114,14 +114,14 @@ class PaymentValueAndDateFormProviderSpec extends SpecBase
       form = form,
       key = paymentDateKey,
       min = stubMin,
-      formError = FormError(paymentDateKey, messages("paymentValueAndDate.date.error.outsideRelevantTaxYear"))
+      formError = FormError(paymentDateKey, messages("paymentValueAndDate.date.error.outsideRelevantTaxYear", "2022", "2023"))
     )
 
     behave like dateFieldWithMax(
       form = form,
       key = paymentDateKey,
       max = stubMax,
-      formError = FormError(paymentDateKey, messages("paymentValueAndDate.date.error.outsideRelevantTaxYear"))
+      formError = FormError(paymentDateKey, messages("paymentValueAndDate.date.error.outsideRelevantTaxYear", "2022", "2023"))
     )
   }
 }
