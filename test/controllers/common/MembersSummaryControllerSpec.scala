2e29daaa22ba1736126972bfd0356bf2f494260c
Shehzad Ismail <shehzad.ismail@atos.net>
2023-04-20 12:55:09 +0100

    [PODS-8141: WIP MembersSummaryControllerSpec]

--- test/controllers/common/MembersSummaryControllerSpec.scala
+++ test/controllers/common/MembersSummaryControllerSpec.scala
@@ -21,25 +21,29 @@ import connectors.UserAnswersCacheConnector
 import data.SampleData
 import data.SampleData._
 import forms.common.MembersSummaryFormProvider
-import models.UserAnswers
+import helpers.DateHelper
+import models.{TaxYear, UserAnswers}
+import models.common.ChooseTaxYear
 import models.enumeration.EventType
 import models.enumeration.EventType.{Event2, Event22, Event23, Event3, Event4, Event5, Event6, Event8, Event8A}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
 import org.scalatest.BeforeAndAfterEach
 import org.scalatestplus.mockito.MockitoSugar
-import pages.EmptyWaypoints
-import pages.common.MembersSummaryPage
+import pages.{EmptyWaypoints, TaxYearPage}
+import pages.common.{ChooseTaxYearPage, MembersDetailsPage, MembersSummaryPage, TotalPensionAmountsPage}
 import play.api.data.Form
 import play.api.inject.bind
 import play.api.inject.guice.GuiceableModule
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import services.EventPaginationService
+import services.EventPaginationService.PaginationStats
 import uk.gov.hmrc.govukfrontend.views.Aliases.{ActionItem, Actions, Text}
 import viewmodels.{Message, SummaryListRowWithTwoValues}
 import views.html.common.{MembersSummaryView, MembersSummaryViewWithPagination}
 
+import java.time.LocalDate
 import scala.concurrent.Future
 
 class MembersSummaryControllerSpec extends SpecBase with BeforeAndAfterEach with MockitoSugar {
@@ -48,8 +52,12 @@ class MembersSummaryControllerSpec extends SpecBase with BeforeAndAfterEach with
 
   private val formProvider = new MembersSummaryFormProvider()
   private val mockUserAnswersCacheConnector = mock[UserAnswersCacheConnector]
-
   private val mockEventPaginationService = mock[EventPaginationService]
+
+  private def fake26MappedMembers(href: String): Seq[SummaryListRowWithTwoValues] = fakeXMappedMembers(26, href)
+
+  private val mockTaxYear = mock[DateHelper]
+  private val validAnswer = LocalDate.of(2022, 5, 12)
 
   private def getRoute(eventType: EventType): String = routes.MembersSummaryController.onPageLoad(waypoints, eventType).url
 
@@ -58,48 +66,57 @@ class MembersSummaryControllerSpec extends SpecBase with BeforeAndAfterEach with
   private def postRoute(eventType: EventType): String = routes.MembersSummaryController.onSubmit(waypoints, eventType).url
 
   private val extraModules: Seq[GuiceableModule] = Seq[GuiceableModule](
-    bind[UserAnswersCacheConnector].toInstance(mockUserAnswersCacheConnector)
+    bind[UserAnswersCacheConnector].toInstance(mockUserAnswersCacheConnector),
+    bind[EventPaginationService].toInstance(mockEventPaginationService),
+    bind[DateHelper].toInstance(mockTaxYear)
   )
 
   override def beforeEach(): Unit = {
     super.beforeEach()
     reset(mockUserAnswersCacheConnector)
+    when(mockTaxYear.now).thenReturn(validAnswer)
     reset(mockEventPaginationService)
   }
 
   "MembersSummary Controller" - {
-    testSuite(formProvider(Event2), Event2, sampleMemberJourneyDataEvent2, SampleData.amountPaid.toString(),
-      controllers.event2.routes.Event2CheckYourAnswersController.onPageLoad(0).url, "999.11")
+    //    testSuite(formProvider(Event2), Event2, sampleMemberJourneyDataEvent2, SampleData.amountPaid.toString(),
+    //      controllers.event2.routes.Event2CheckYourAnswersController.onPageLoad(0).url, "999.11")
+    //
+    //    testSuite(formProvider(Event3), Event3, sampleMemberJourneyDataEvent3and4and5(Event3), SampleData.paymentDetailsCommon.amountPaid.toString(),
+    //      controllers.event3.routes.Event3CheckYourAnswersController.onPageLoad(0).url, "54.23")
+    //
+    //    testSuite(formProvider(Event4), Event4, sampleMemberJourneyDataEvent3and4and5(Event4), SampleData.paymentDetailsCommon.amountPaid.toString(),
+    //      controllers.event4.routes.Event4CheckYourAnswersController.onPageLoad(0).url, "54.23")
+    //
+    //    testSuite(formProvider(Event5), Event5, sampleMemberJourneyDataEvent3and4and5(Event5), SampleData.paymentDetailsCommon.amountPaid.toString(),
+    //      controllers.event5.routes.Event5CheckYourAnswersController.onPageLoad(0).url, "54.23")
+    //
+    //    testSuite(formProvider(Event6), Event6, sampleMemberJourneyDataEvent6, SampleData.crystallisedDetails.amountCrystallised.toString(),
+    //      controllers.event6.routes.Event6CheckYourAnswersController.onPageLoad(0).url, "857.12")
+    //
+    //    testSuite(formProvider(Event8), Event8, sampleMemberJourneyDataEvent8, SampleData.lumpSumDetails.lumpSumAmount.toString(),
+    //      controllers.event8.routes.Event8CheckYourAnswersController.onPageLoad(0).url, "223.11")
+    //
+    //    testSuite(formProvider(Event8A), Event8A, sampleMemberJourneyDataEvent8A, SampleData.lumpSumDetails.lumpSumAmount.toString(),
+    //      controllers.event8a.routes.Event8ACheckYourAnswersController.onPageLoad(0).url, "223.11")
+    //
+    //    testSuite(formProvider(Event22), Event22, sampleMemberJourneyDataEvent22, SampleData.totalPaymentAmount.toString(),
+    //      controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad(0).url, "999.11")
 
-    testSuite(formProvider(Event3), Event3, sampleMemberJourneyDataEvent3and4and5(Event3), SampleData.paymentDetailsCommon.amountPaid.toString(),
-      controllers.event3.routes.Event3CheckYourAnswersController.onPageLoad(0).url, "54.23")
+    //    testSuite(formProvider(Event23), Event23, sampleMemberJourneyDataEvent23, SampleData.totalPaymentAmountEvent23CurrencyFormat,
+    //      controllers.event23.routes.Event23CheckYourAnswersController.onPageLoad(0).url, "1,234.56")
 
-    testSuite(formProvider(Event4), Event4, sampleMemberJourneyDataEvent3and4and5(Event4), SampleData.paymentDetailsCommon.amountPaid.toString(),
-      controllers.event4.routes.Event4CheckYourAnswersController.onPageLoad(0).url, "54.23")
-
-    testSuite(formProvider(Event5), Event5, sampleMemberJourneyDataEvent3and4and5(Event5), SampleData.paymentDetailsCommon.amountPaid.toString(),
-      controllers.event5.routes.Event5CheckYourAnswersController.onPageLoad(0).url, "54.23")
-
-    testSuite(formProvider(Event6), Event6, sampleMemberJourneyDataEvent6, SampleData.crystallisedDetails.amountCrystallised.toString(),
-      controllers.event6.routes.Event6CheckYourAnswersController.onPageLoad(0).url, "857.12")
-
-    testSuite(formProvider(Event8), Event8, sampleMemberJourneyDataEvent8, SampleData.lumpSumDetails.lumpSumAmount.toString(),
-      controllers.event8.routes.Event8CheckYourAnswersController.onPageLoad(0).url, "223.11")
-
-    testSuite(formProvider(Event8A), Event8A, sampleMemberJourneyDataEvent8A, SampleData.lumpSumDetails.lumpSumAmount.toString(),
-      controllers.event8a.routes.Event8ACheckYourAnswersController.onPageLoad(0).url, "223.11")
-
-    testSuite(formProvider(Event22), Event22, sampleMemberJourneyDataEvent22, SampleData.totalPaymentAmount.toString(),
-      controllers.event22.routes.Event22CheckYourAnswersController.onPageLoad(0).url, "999.11")
-
-    testSuite(formProvider(Event23), Event23, sampleMemberJourneyDataEvent23, SampleData.totalPaymentAmountEvent23CurrencyFormat,
-      controllers.event23.routes.Event23CheckYourAnswersController.onPageLoad(0).url, "1,234.56")
+    testSuiteWithPagination(formProvider(Event23), Event23, controllers.event23.routes.Event23CheckYourAnswersController.onPageLoad(0).url, "260.00")
   }
 
   private def testSuite(form: Form[Boolean], eventType: EventType, sampleData: UserAnswers, secondValue: String, href: String, arbitraryAmount: String): Unit = {
     testReturnOkAndCorrectView(eventType, form, sampleData, secondValue, href, arbitraryAmount)
     testSaveAnswerAndRedirectWhenValid(eventType)
     testBadRequestForInvalidDataSubmission(eventType, form)
+  }
+
+  private def testSuiteWithPagination(form: Form[Boolean], eventType: EventType, href: String, arbitraryAmount: String): Unit = {
+    testReturnOkAndCorrectViewWithPagination(eventType, form, href, arbitraryAmount)
   }
 
   private def testReturnOkAndCorrectView(eventType: EventType, form: Form[Boolean], sampleData: UserAnswers, secondValue: String, href: String, arbitraryAmount: String): Unit = {
@@ -140,44 +157,36 @@ class MembersSummaryControllerSpec extends SpecBase with BeforeAndAfterEach with
     }
   }
 
-//  private def testReturnOkAndCorrectViewWithPagination(eventType: EventType, form: Form[Boolean], sampleData: UserAnswers, secondValue: String, href: String, arbitraryAmount: String): Unit = {
-//    s"must return OK and the correct view for a GET for Event $eventType" in {
-//
-//      val application = applicationBuilder(userAnswers = Some(sampleData)).build()
-//
-//      running(application) {
-//        val request = FakeRequest(GET, getRouteWithPagination(eventType))
-//
-//        val result = route(application, request).value
-//
-//        val view = application.injector.instanceOf[MembersSummaryViewWithPagination]
-//
-//        val expectedSeq =
-//          Seq(
-//            SummaryListRowWithTwoValues(
-//              key = SampleData.memberDetails.fullName,
-//              firstValue = SampleData.memberDetails.nino,
-//              secondValue = secondValue,
-//              actions = Some(Actions(
-//                items = Seq(
-//                  ActionItem(
-//                    content = Text(Message("site.view")),
-//                    href = href
-//                  ),
-//                  ActionItem(
-//                    content = Text(Message("site.remove")),
-//                    href = "#"
-//                  )
-//                )
-//              ))
-//            ))
-//
-//        status(result) mustEqual OK
-//        contentAsString(result) mustEqual view(form, waypoints, eventType, expectedSeq, arbitraryAmount, "2023", 0)(request, messages(application)).toString
-//      }
-//    }
-//  }
+  private def testReturnOkAndCorrectViewWithPagination(eventType: EventType, form: Form[Boolean], href: String, arbitraryAmount: String): Unit = {
+    when(mockEventPaginationService.paginateMappedMembers(any(), any())).thenReturn(paginationStats26Members(href))
 
+    s"must return OK and the correct view with pagination for a GET for Event $eventType" in {
+
+      val sampleData =
+        (0 to 25).foldLeft(emptyUserAnswersWithTaxYear) { (acc, i) =>
+          acc.setOrException(MembersDetailsPage(Event23, i), memberDetails)
+            .setOrException(ChooseTaxYearPage(Event23, i), ChooseTaxYear("2015"))
+            .setOrException(TotalPensionAmountsPage(Event23, i), BigDecimal(10.00))
+        }
+
+      val application = applicationBuilder(userAnswers = Some(sampleData)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, getRouteWithPagination(eventType))
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[MembersSummaryViewWithPagination]
+
+        val expectedPaginationStats = PaginationStats(fake26MappedMembers(href), 26, 2, (1, 25), Nil)
+
+        status(result) mustEqual OK
+
+        contentAsString(result) mustEqual view(form, waypoints, eventType, fake26MappedMembers(href), arbitraryAmount, "2023", expectedPaginationStats, 0)(request, messages(application)).toString
+      }
+
+    }
+  }
 
   private def testSaveAnswerAndRedirectWhenValid(eventType: EventType): Unit = {
     s"must save the answer and redirect to the next page when valid data is submitted for Event $eventType" in {
@@ -225,4 +234,44 @@ class MembersSummaryControllerSpec extends SpecBase with BeforeAndAfterEach with
       }
     }
   }
+
+  private def fakeXMappedMembers(x: Int, href: String): Seq[SummaryListRowWithTwoValues] = for {
+    i <- 1 to x
+  } yield {
+    SummaryListRowWithTwoValues(s"${memberDetails.fullName}", s"${memberDetails.nino}", "10.00",
+      actions = Some(Actions(
+        items = Seq(
+          ActionItem(
+            content = Text(Message("site.view")),
+            href = href
+          ),
+          ActionItem(
+            content = Text(Message("site.remove")),
+            href = "#"
+          )
+        )
+      )))
+  }
+
+  private def fakePaginationStats(
+                                   slicedMems: Seq[SummaryListRowWithTwoValues],
+                                   totMems: Int,
+                                   totPages: Int,
+                                   pagStartEnd: (Int, Int),
+                                   pagSeq: Seq[Int]
+                                 ): PaginationStats = PaginationStats(
+    slicedMembers = slicedMems,
+    totalNumberOfMembers = totMems,
+    totalNumberOfPages = totPages,
+    pageStartAndEnd = pagStartEnd,
+    pagerSeq = pagSeq
+  )
+
+  private def paginationStats26Members(href: String) = fakePaginationStats(
+    fake26MappedMembers(href).slice(0, 25),
+    26,
+    2,
+    (1, 25),
+    1 to 2
+  )
 }
