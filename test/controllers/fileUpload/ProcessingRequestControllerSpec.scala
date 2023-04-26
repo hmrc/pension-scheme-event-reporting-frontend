462a0f84daa108e3e378c8eaf8ccbf70f2e4a509
brownbonnie <bonnie.brown@digital.hmrc.gov.uk>
2023-04-26 15:24:17 +0100

    PODS-8125: WIP on file being processed page for file upload

---
+++ test/controllers/fileUpload/ProcessingRequestControllerSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.fileUpload
+
+import base.SpecBase
+import models.enumeration.EventType
+import pages.EmptyWaypoints
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import views.html.fileUpload.ProcessingRequestView
+
+class ProcessingRequestControllerSpec extends SpecBase {
+
+  private val waypoints = EmptyWaypoints
+  private val event22 = EventType.Event22
+
+  "Processing Request Controller" - {
+
+    // TODO Is this the right way to test this? Will be used for multiple events
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+
+        val request = FakeRequest(GET, routes.ProcessingRequestController.onPageLoad(waypoints, event22).url)
+
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+        println(routes.ProcessingRequestController.onPageLoad(waypoints, event22).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[ProcessingRequestView]
+
+        println("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy")
+        println(view(continueUrl = controllers.fileUpload.routes.ProcessingRequestController.onPageLoad(waypoints, event22).url)(request, messages(application)).toString)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(controllers.IndexController.onPageLoad.url)(request, messages(application)).toString
+      }
+    }
+  }
+}
