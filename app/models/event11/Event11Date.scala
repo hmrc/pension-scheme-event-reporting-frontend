a3878c21b5a7e4e3bb6e303f23583876e618641b
ngmjohnstone <ngmjohnstone@yahoo.com>
2023-05-16 10:16:44 +0100

    PODS-8342: Changed date format

--- app/models/event11/Event11Date.scala
+++ app/models/event11/Event11Date.scala
@@ -23,7 +23,7 @@ import java.time.format.DateTimeFormatter
 
 case class Event11Date(date: LocalDate) {
   def formatEvent11Date: String = {
-    date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+    date.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
   }
 }
 
