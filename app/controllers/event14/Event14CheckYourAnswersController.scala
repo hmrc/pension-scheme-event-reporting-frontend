6748b68cb6bcf8c2d48a6ad8684d17d0f5990c1e
emrawsy <27211889+emrawsy@users.noreply.github.com>
2023-05-16 09:49:21 +0100

    PODS-8345 uncommenting compile

--- app/controllers/event14/Event14CheckYourAnswersController.scala
+++ app/controllers/event14/Event14CheckYourAnswersController.scala
@@ -52,15 +52,12 @@ class Event14CheckYourAnswersController @Inject()(
     }
 
   def onClick: Action[AnyContent] =
-    (identify andThen getData(Event14) andThen requireData)/*.async*/  { implicit request =>
+    (identify andThen getData(Event14) andThen requireData).async  { implicit request =>
       val waypoints = EmptyWaypoints
-     /*TODO uncomment this once backend compile is fixed
       connector.compileEvent(request.pstr, Event14).map {
         _ =>
-
-      */
         Redirect(controllers.routes.EventSummaryController.onPageLoad(waypoints).url)
-      //}
+      }
     }
 
   private def buildEvent14CYARows(waypoints: Waypoints, sourcePage: CheckAnswersPage)
