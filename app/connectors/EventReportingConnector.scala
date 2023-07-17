b0dce04434413ea827accf6cdf593def8b43de5c
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2023-07-17 11:46:25 +0100

    PODS-8490: compile is now after version change

--- app/connectors/EventReportingConnector.scala
+++ app/connectors/EventReportingConnector.scala
@@ -92,6 +92,7 @@ class EventReportingConnector @Inject()(
         response.status match {
           case NO_CONTENT => ()
           case _ =>
+            println(response.body, response.status)
             throw new HttpException(response.body, response.status)
         }
       }
