7ad3f611f1cdd15864e26282b59e815ca3470404
Shehzad Ismail <shehzad.ismail@atos.net>
2023-07-19 17:07:51 +0100

    [PODS-8588: WIP Event1ValidatorSpec updated header]

--- test/services/fileUpload/Event1ValidatorSpec.scala
+++ test/services/fileUpload/Event1ValidatorSpec.scala
@@ -42,9 +42,6 @@ import pages.event1._
 import pages.event1.member.{BenefitInKindBriefDescriptionPage, PaymentNaturePage}
 import play.api.libs.json.Json
 import services.fileUpload.ValidatorErrorMessages.HeaderInvalidOrFileIsEmpty
-import utils.DateHelper
-
-import java.time.LocalDate
 
 class Event1ValidatorSpec extends SpecBase with Matchers with MockitoSugar with BeforeAndAfterEach {
   //scalastyle:off magic.number
@@ -91,31 +88,50 @@ class Event1ValidatorSpec extends SpecBase with Matchers with MockitoSugar with 
       ))
     }
 
-//    "return validation errors when present, including tax year in future" in {
-//      DateHelper.setDate(Some(LocalDate.of(2023, 6, 1)))
-//      val csvFile = CSVParser.split(
-//        s"""$header
-//            member,Joe,Bloggs,AA234567D,YES,YES,YES,,,,Benefit,Description,,,,,,,,,,,,,1000.00,08/11/2022"""
-//
-//      )
-//      val ua = UserAnswers().setOrException(TaxYearPage, TaxYear("2022"), nonEventTypeData = true)
-//
-//      val result = validator.validate(csvFile, ua)
-//      result mustBe Invalid(Seq(
-//        ValidationError(1, 1, "membersDetails.error.lastName.required", "lastName"),
-//        ValidationError(2, 6, "Date must be between 06 April 2006 and 05 April 2024", "crystallisedDate")
-//      ))
-//    }
+    //    "return validation errors when present, including tax year in future" in {
+    //      DateHelper.setDate(Some(LocalDate.of(2022, 6, 1)))
+    //      val csvFile = CSVParser.split(
+    //        s"""$header
+    //            member,,Bloggs,AA234567D,YES,YES,YES,,,,Benefit,Description,,,,,,,,,,,,,1000.00,08/11/2022"""
+    //
+    //      )
+    //      val ua = UserAnswers().setOrException(TaxYearPage, TaxYear("2022"), nonEventTypeData = true)
+    //
+    //      val result = validator.validate(csvFile, ua)
+    //      result mustBe Invalid(Seq(
+    //        ValidationError(1, 2, "membersDetails.error.firstName.required", "firstName")
+    //      ))
+    //    }
   }
 
 }
 
 object Event1ValidatorSpec {
-  private val header = "Members first name,Members last name,Members National Insurance number," +
-    "The type of protection held for the crystallisation (see members LTA protection certificate)," +
-    "Members protection reference (see members LTA protection certificate)," +
-    "Total amount crystallised (£)," +
-    "Date of the benefit crystallisation (XX/XX/XXXX)"
+  private val header = "Member or employer," +
+    "Member: first name,Member: last name,Member: National Insurance number," +
+    "Member: Do you hold a signed mandate from the member to deduct tax from their unauthorised payment? (YES/NO)," +
+    "Member: Is the value of the unauthorised payment more than 25% of the pension fund for the individual? (YES/NO)," +
+    "Member: Is the scheme paying the unauthorised payment surcharge on behalf of the member? (YES/NO)," +
+    "Employer: company or organisation name," +
+    "Employer: company number," +
+    "Employer: company address," +
+    "Member and employer: Nature of the unauthorised payment or deemed unauthorised payment (see instructions for details)," +
+    "If BENEFIT: Give a brief description (up to 150 characters)," +
+    "If COURT: What is the name of the person or organisation that received the unauthorised payment? (see instructions for details)," +
+    "If EARLY: Give a brief description (up to 150 characters)," +
+    "If ERROR: Give a brief description (up to 150 characters)," +
+    "If LOANS: Amount of the loan (£)," +
+    "If LOANS: Value of the fund (£)," +
+    "If OTHER: Give a brief description (up to 150 characters)," +
+    "If OVERPAYMENT: What is the reason for the overpayment/write off? (see instructions for details)," +
+    "If REFUND: Who received the fund? (see instructions for details)," +
+    "If RESIDENTIAL: What is the address of the residential property? (see instructions for details)," +
+    "If TANGIBLE: Give a brief description (up to 150 characters)," +
+    "If TRANSFER: Who was the transfer was made to? (see instructions for details)," +
+    "If TRANSFER: What are the scheme details? (see instructions for details)," +
+    "Member and Employer: Total value or amount of the unauthorised payment (£)," +
+    "Member and employer: Date of payment or when benefit made available (see instructions for details)"
+
   private val mockFrontendAppConfig = mock[FrontendAppConfig]
 
   private val whoReceivedUnauthPaymentFormProvider = new WhoReceivedUnauthPaymentFormProvider
