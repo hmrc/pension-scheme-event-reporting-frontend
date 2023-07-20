4e1a5aa39f1825ea1447e4faef5e8c351881d1f4
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-07-20 07:56:51 +0100

    PODS-8603 WIP

--- test/controllers/EventSummaryControllerSpec.scala
+++ test/controllers/EventSummaryControllerSpec.scala
@@ -21,7 +21,7 @@ import connectors.EventReportingConnector
 import forms.EventSummaryFormProvider
 import models.enumeration.EventType
 import models.enumeration.VersionStatus.Compiled
-import models.{TaxYear, UserAnswers, VersionInfo}
+import models.{EventSummary, TaxYear, UserAnswers, VersionInfo}
 import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito._
@@ -62,7 +62,7 @@ class EventSummaryControllerSpec extends SpecBase with SummaryListFluency with B
         .setOrException(TaxYearPage, TaxYear("2022"))
         .setOrException(VersionInfoPage, VersionInfo(1, Compiled))
 
-      val seqOfEvents = Seq(EventType.Event1, EventType.Event18)
+      val seqOfEvents = Seq(EventSummary(EventType.Event1, 1), EventSummary(EventType.Event18, 1))
 
       when(mockEventReportSummaryConnector.getEventReportSummary(any(), ArgumentMatchers.eq("2022-04-06"), ArgumentMatchers.eq(1))(any(), any())).thenReturn(
         Future.successful(seqOfEvents)
