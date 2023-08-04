c7ac56ccd003f8aa27ab15624fcd646d35ee7f0a
Shehzad Ismail <shehzad.ismail@atos.net>
2023-08-04 12:24:33 +0100

    [PODS-8662: Fixed test for DeclarationPspControllerSpec]

--- test/controllers/DeclarationPspControllerSpec.scala
+++ test/controllers/DeclarationPspControllerSpec.scala
@@ -18,7 +18,7 @@ package controllers
 
 import base.SpecBase
 import connectors.MinimalConnector.{IndividualDetails, MinimalDetails}
-import connectors.{EventReportingConnector, MinimalConnector, SchemeDetailsConnector, UserAnswersCacheConnector}
+import connectors.{MinimalConnector, SchemeDetailsConnector, UserAnswersCacheConnector}
 import data.SampleData.sampleEvent20JourneyData
 import forms.DeclarationPspFormProvider
 import models.{SchemeDetails, UserAnswers}
@@ -29,8 +29,10 @@ import org.scalatestplus.mockito.MockitoSugar.mock
 import pages.{DeclarationPspPage, EmptyWaypoints}
 import play.api.inject.bind
 import play.api.inject.guice.GuiceableModule
+import play.api.mvc.Results.Ok
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
+import services.SubmitService
 import views.html.DeclarationPspView
 
 import scala.concurrent.Future
@@ -46,14 +48,16 @@ class DeclarationPspControllerSpec extends SpecBase with BeforeAndAfterEach {
 
   private val mockUserAnswersCacheConnector = mock[UserAnswersCacheConnector]
   private val mockSchemeDetailsConnector = mock[SchemeDetailsConnector]
-  private val mockEventReportingConnector = mock[EventReportingConnector]
+  private val mockSubmitService = mock[SubmitService]
   private val mockMinimalConnector = mock[MinimalConnector]
   private val mockSchemeDetails = SchemeDetails("schemeName", "87219363YN", "Open", authorisingPsaId)
   val testEmail = "test@test.com"
   val mockMinimalDetails: MinimalDetails = {
-    MinimalDetails(testEmail, false, None, Some(IndividualDetails(firstName = "John", None,  lastName = "Smith")), false, false)
+    MinimalDetails(testEmail, false, None, Some(IndividualDetails(firstName = "John", None, lastName = "Smith")), false, false)
   }
+
   private def getRoute: String = routes.DeclarationPspController.onPageLoad(waypoints).url
+
   private def postRoute: String = routes.DeclarationPspController.onSubmit(waypoints).url
 
   private val validValue = "abc"
@@ -61,14 +65,14 @@ class DeclarationPspControllerSpec extends SpecBase with BeforeAndAfterEach {
   override def beforeEach(): Unit = {
     reset(mockUserAnswersCacheConnector)
     reset(mockSchemeDetailsConnector)
-    reset(mockEventReportingConnector)
+    reset(mockSubmitService)
     reset(mockMinimalConnector)
   }
 
   private val extraModules: Seq[GuiceableModule] = Seq[GuiceableModule](
     bind[UserAnswersCacheConnector].toInstance(mockUserAnswersCacheConnector),
     bind[SchemeDetailsConnector].toInstance(mockSchemeDetailsConnector),
-    bind[EventReportingConnector].toInstance(mockEventReportingConnector),
+    bind[SubmitService].toInstance(mockSubmitService),
     bind[MinimalConnector].toInstance(mockMinimalConnector)
   )
 
@@ -114,7 +118,7 @@ class DeclarationPspControllerSpec extends SpecBase with BeforeAndAfterEach {
       when(mockSchemeDetailsConnector.getPspSchemeDetails(any(), any())(any(), any())).thenReturn(Future.successful(mockSchemeDetails))
       when(mockUserAnswersCacheConnector.save(any(), any())(any(), any()))
         .thenReturn(Future.successful(()))
-      when(mockEventReportingConnector.submitReport(any(), any(), any())(any(), any())).thenReturn(Future.successful(()))
+      when(mockSubmitService.submitReport(any(), any())(any(), any())).thenReturn(Future.successful(Ok))
 
       val application =
         applicationBuilder(userAnswers = Some(sampleEvent20JourneyData), extraModules)
