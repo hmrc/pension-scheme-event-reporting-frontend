4cd70de302254dfb68dd6c522d691bc2e8075e6a
ngmjohnstone <ngmjohnstone@yahoo.com>
2023-10-17 14:53:49 +0100

    PODS-8653: Refactoring addressing TODO

--- app/forms/address/ManualAddressFormProvider.scala
+++ app/forms/address/ManualAddressFormProvider.scala
@@ -16,7 +16,7 @@
 
 package forms.address
 
-import forms.address.ManualAddressFormProvider.addressLineStr
+import forms.address.ManualAddressFormProvider._
 import forms.mappings.AddressMapping
 import models.address.Address
 import play.api.data.Form
@@ -26,50 +26,31 @@ import utils.CountryOptions
 
 import javax.inject.Inject
 
-// TODO: refactor
 class ManualAddressFormProvider @Inject()(countryOptions: CountryOptions) extends AddressMapping {
-  //scalastyle:off
-  def apply(companyName: String)(implicit messages: Messages): Form[Address] = Form(
-    mapping(
-      // TODO: refactor all below to reuse common component.
-      addressLineStr(1) ->
-        addressLineMapping(
-          messages(s"address.${addressLineStr(1)}.error.required", companyName),
-          messages(s"address.${addressLineStr(1)}.error.length", companyName),
-          messages(s"address.${addressLineStr(1)}.error.invalid", companyName)
-        ),
-      addressLineStr(2) ->
-        addressLineMapping(
-          messages(s"address.${addressLineStr(2)}.error.required", companyName),
-          messages(s"address.${addressLineStr(2)}.error.length", companyName),
-          messages(s"address.${addressLineStr(2)}.error.invalid", companyName)
-        ),
-      addressLineStr(3) ->
-        optionalAddressLineMapping(
-          messages("address.addressLine3.error.length", companyName),
-          messages("address.addressLine3.error.invalid", companyName)
-        ),
-      addressLineStr(4) ->
-        optionalAddressLineMapping(
-          messages("address.addressLine4.error.length", companyName),
-          messages("address.addressLine4.error.invalid", companyName)
-        ),
-      "postCode" ->
-        postCodeWithCountryMapping(
-          messages("address.postCode.error.required", companyName),
-          messages("enterPostcode.error.invalid", companyName),
-          "enterPostcode.error.nonUKLength"
-        ),
-      "country" ->
-        countryMapping
-        (countryOptions,
-          messages("address.country.error.required", companyName),
-          "address.country.error.invalid"
-        )
-    )(Address.apply)(Address.unapply)
-  )
+  def apply(companyName: String)(implicit messages: Messages): Form[Address] =
+    Form(
+      mapping(
+        addressLines._1 -> addressLineMapping(requiredAddressLineArgs(addressLines._1, companyName)),
+        addressLines._2 -> addressLineMapping(requiredAddressLineArgs(addressLines._2, companyName)),
+        addressLines._3 -> optionalAddressLineMapping(optionalAddressLineArgs(addressLines._3, companyName)),
+        addressLines._4 -> optionalAddressLineMapping(optionalAddressLineArgs(addressLines._4, companyName)),
+        postCodeAndCountry._1 -> postCodeWithCountryMapping(postCodeArgs(postCodeAndCountry._1, companyName)),
+        postCodeAndCountry._2 -> countryMapping(countryOptions, countryArgs(postCodeAndCountry._2, companyName))
+      )(Address.apply)(Address.unapply)
+    )
 }
 
 object ManualAddressFormProvider {
-  val addressLineStr: Int => String = (int: Int) => s"addressLine$int"
+  private val addressLines: (String, String, String, String) = ("addressLine1", "addressLine2", "addressLine3", "addressLine4")
+  private val postCodeAndCountry: (String, String) = ("postCode", "country")
+  private val errMsg: (String, String) => String = (key: String, err: String) => s"address.$key.error.$err"
+  private def requiredAddressLineArgs(key: String, companyName: String)(implicit messages: Messages): (String, String, String) =
+    (messages(errMsg(key, "required"), companyName), messages(errMsg(key, "length"), companyName), messages(errMsg(key, "invalid"), companyName))
+  private def optionalAddressLineArgs(key: String, companyName: String)(implicit messages: Messages): (String, String) =
+    (messages(errMsg(key, "length"), companyName), messages(errMsg(key, "invalid"), companyName))
+  private def postCodeArgs(key: String, companyName: String)(implicit messages: Messages): (String, String, String) =
+    (messages(errMsg(key, "required"), companyName), messages(errMsg(key, "invalid"), companyName), "enterPostcode.error.nonUKLength")
+  private def countryArgs(key: String, companyName: String)(implicit messages: Messages): (String, String) =
+    (messages(errMsg(key, "required"), companyName), errMsg(key, "invalid"))
+
 }
