1f44df09fe7b66aadff084bfd1099ad27526d1e4
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2023-11-09 09:27:13 +0000

    PODS-8860 - CYA page missing answers check WIP

--- app/controllers/event2/Event2CheckYourAnswersController.scala
+++ app/controllers/event2/Event2CheckYourAnswersController.scala
@@ -19,23 +19,26 @@ package controllers.event2
 import com.google.inject.Inject
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
 import helpers.ReadOnlyCYA
-import models.{Index, MemberSummaryPath}
 import models.enumeration.EventType.Event2
 import models.requests.DataRequest
-import pages.event2.Event2CheckYourAnswersPage
-import pages.{CheckAnswersPage, EmptyWaypoints, VersionInfoPage, Waypoints}
+import models.{Index, MemberSummaryPath}
+import pages.common.MembersDetailsPage
+import pages.event2.{AmountPaidPage, DatePaidPage, Event2CheckYourAnswersPage}
+import pages.{CheckAnswersPage, EmptyWaypoints, QuestionPage, VersionInfoPage, Waypoints}
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.Reads
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.CompileService
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.Event2MemberPageNumbers
+import utils.MissingAnswersCheck.checkAnAnswerIsPresent
 import viewmodels.checkAnswers.MembersDetailsSummary
 import viewmodels.event2.checkAnswers.{AmountPaidSummary, DatePaidSummary}
 import viewmodels.govuk.summarylist._
 import views.html.CheckYourAnswersView
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class Event2CheckYourAnswersController @Inject()(
                                                   override val messagesApi: MessagesApi,
@@ -51,17 +54,30 @@ class Event2CheckYourAnswersController @Inject()(
     (identify andThen getData(Event2) andThen requireData) { implicit request =>
       val thisPage = Event2CheckYourAnswersPage(index)
       val waypoints = EmptyWaypoints
-      val continueUrl = controllers.event2.routes.Event2CheckYourAnswersController.onClick.url
+      val continueUrl = controllers.event2.routes.Event2CheckYourAnswersController.onClick(index).url
       val version = request.userAnswers.get(VersionInfoPage).map(_.version)
       val readOnlyHeading = ReadOnlyCYA.readOnlyHeading(Event2, version, request.readOnly())
       Ok(view(SummaryListViewModel(rows = buildEvent2CYARows(waypoints, thisPage, index)), continueUrl, readOnlyHeading))
     }
 
-  def onClick: Action[AnyContent] =
+  def onClick(index: Index)(implicit request: DataRequest[AnyContent], rds: Reads[Any]): Action[AnyContent] =
     (identify andThen getData(Event2) andThen requireData).async { implicit request =>
-      compileService.compileEvent(Event2, request.pstr, request.userAnswers).map {
-        _ =>
-          Redirect(controllers.common.routes.MembersSummaryController.onPageLoad(EmptyWaypoints, MemberSummaryPath(Event2)).url)
+      val pagesControllerPairs: Map[QuestionPage[_], String] = Map(
+        MembersDetailsPage(Event2, index, Event2MemberPageNumbers.FIRST_PAGE_DECEASED) ->
+          controllers.common.routes.MembersDetailsController.onPageLoad(EmptyWaypoints, Event2, index, Event2MemberPageNumbers.FIRST_PAGE_DECEASED).url,
+        MembersDetailsPage(Event2, index, Event2MemberPageNumbers.SECOND_PAGE_BENEFICIARY) ->
+          controllers.common.routes.MembersDetailsController.onPageLoad(EmptyWaypoints, Event2, index, Event2MemberPageNumbers.SECOND_PAGE_BENEFICIARY).url,
+        AmountPaidPage(index, Event2) -> controllers.event2.routes.AmountPaidController.onPageLoad(EmptyWaypoints, index).url,
+        DatePaidPage(index, Event2) -> controllers.event2.routes.DatePaidController.onPageLoad(EmptyWaypoints, index).url)
+
+      if (checkAllAnswersPresent(pagesControllerPairs).isEmpty) {
+        compileService.compileEvent(Event2, request.pstr, request.userAnswers).map {
+          _ =>
+            Redirect(controllers.common.routes.MembersSummaryController.onPageLoad(EmptyWaypoints, MemberSummaryPath(Event2)).url)
+        }
+      } else {
+        val missingAnswersPage = checkAllAnswersPresent(pagesControllerPairs).head
+        Future.successful(Redirect(getUrl(missingAnswersPage, index)))
       }
     }
 
@@ -78,5 +94,29 @@ class Event2CheckYourAnswersController @Inject()(
       AmountPaidSummary.row(request.userAnswers, waypoints, sourcePage, request.readOnly(), index).toSeq ++
       DatePaidSummary.row(request.userAnswers, waypoints, sourcePage, request.readOnly(), index).toSeq
   }
-}
 
+//  private def checkAllAnswersPresent(index: Index)(implicit request: DataRequest[AnyContent]): Seq[QuestionPage[_]] = {
+//    Seq(checkAnAnswerIsPresent(MembersDetailsPage(Event2, index, Event2MemberPageNumbers.FIRST_PAGE_DECEASED)),
+//      checkAnAnswerIsPresent(MembersDetailsPage(Event2, index, Event2MemberPageNumbers.SECOND_PAGE_BENEFICIARY)),
+//      checkAnAnswerIsPresent(AmountPaidPage(index, Event2)),
+//      checkAnAnswerIsPresent(DatePaidPage(index, Event2))
+//    ).flatten
+//  }
+
+  private def checkAllAnswersPresent(pagesControllerPairs: Map[QuestionPage[_], String])
+                                    (implicit request: DataRequest[AnyContent], reads: Reads[Any]): Seq[QuestionPage[_]] = {
+    val pages = pagesControllerPairs.keys.toSeq
+    pages.flatMap(page => checkAnAnswerIsPresent(page))
+  }
+
+  private def getUrl[A](page: QuestionPage[A], index: Index) = {
+    page match {
+      case MembersDetailsPage(_, _, 1) =>
+        controllers.common.routes.MembersDetailsController.onPageLoad(EmptyWaypoints, Event2, index, Event2MemberPageNumbers.FIRST_PAGE_DECEASED).url
+      case MembersDetailsPage(_, _, 2) =>
+        controllers.common.routes.MembersDetailsController.onPageLoad(EmptyWaypoints, Event2, index, Event2MemberPageNumbers.SECOND_PAGE_BENEFICIARY).url
+      case AmountPaidPage(_, _) => controllers.event2.routes.AmountPaidController.onPageLoad(EmptyWaypoints, index).url
+      case DatePaidPage(_, _) => controllers.event2.routes.DatePaidController.onPageLoad(EmptyWaypoints, index).url
+    }
+  }
+}
