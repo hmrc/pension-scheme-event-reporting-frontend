76eb4d43063d45fa5299f29c87ef238dce2d548c
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-01-05 10:03:59 +0000

    PODS-8892 - Improve exception logging when user answers unavailable

--- app/controllers/WantToSubmitController.scala
+++ app/controllers/WantToSubmitController.scala
@@ -56,8 +56,8 @@ class WantToSubmitController @Inject()(
         value => {
           val originalUserAnswers = request.userAnswers.fold(UserAnswers())(identity)
           val updatedAnswers = originalUserAnswers.setOrException(WantToSubmitPage, value)
-          val version = updatedAnswers.get(VersionInfoPage).get.version
           val startYear = s"${TaxYear.getSelectedTaxYear(updatedAnswers).startYear}-04-06"
+          val version = updatedAnswers.get(VersionInfoPage).get.version
           eventReportingConnector.getEventReportSummary(request.pstr, startYear, version)
             .flatMap { seqEventSummaries =>
               val currentCompileEventTypes = seqEventSummaries.filter {
