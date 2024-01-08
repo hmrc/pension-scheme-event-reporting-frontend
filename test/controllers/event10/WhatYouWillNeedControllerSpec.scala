bdacecaf46d8f753d2eec8abbf5597ca26ae4dd3
jamdade-sharad <sharad.jamdade@digital.hmrc.gov.uk>
2024-01-08 18:14:39 +0000

    PODS-8919 - Updated copyright year

--- test/controllers/event10/WhatYouWillNeedControllerSpec.scala
+++ test/controllers/event10/WhatYouWillNeedControllerSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,6 @@ import base.SpecBase
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.event10.WhatYouWillNeedView
-import controllers.event10.routes._
 import pages.EmptyWaypoints
 
 class WhatYouWillNeedControllerSpec extends SpecBase {
