95a5839eaac05777a1a8e7c5c7d096aedfd7b53c
DIGITAL317588 <17904833+smithrebecca@users.noreply.github.com>
2025-09-15 14:52:28 +0100

    DDCNL-10809

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.15.0"
+  private val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
