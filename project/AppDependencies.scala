b9df1ab27479f32b22d063f181d93ebf43737872
DIGITAL317588 <17904833+smithrebecca@users.noreply.github.com>
2025-09-15 14:46:35 +0100

    DDCNL-10809 | bootstrap version upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.15.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
