9b3c4d74d3df87e1efd38eba5c24a9f3869d167c
Balasubrahmanyam <173818211+bsgangapatnam@users.noreply.github.com>
2026-06-18 12:50:11 +0100

    refactor the files to minLength is 1

--- test/forms/event24/EmployerPayeReferenceFormProviderSpec.scala
+++ test/forms/event24/EmployerPayeReferenceFormProviderSpec.scala
@@ -27,7 +27,6 @@ class EmployerPayeReferenceFormProviderSpec extends StringFieldBehaviours with C
   private val lengthErrorKey = "employerPayeReference.event24.error.length"
   private val requiredErrorKey = "employerPayeReference.event24.error.required"
   private val maxLength = 12
-  private val minLength = 5
 
   private val form = new EmployerPayeReferenceFormProvider()()
   val fieldName = "value"
@@ -56,14 +55,6 @@ class EmployerPayeReferenceFormProviderSpec extends StringFieldBehaviours with C
       maxLength = maxLength,
       lengthError = FormError(fieldName, lengthErrorKey, Seq(maxLength))
     )
-
-    behave like fieldWithMinLength(
-      form,
-      fieldName,
-      minLength = minLength,
-      lengthError = FormError(fieldName, lengthErrorKey, Seq(minLength))
-    )
-
 
     behave like mandatoryField(
       form,
